27775c10a9b18e39b7fc4196e1e4a5845a290a27
rihan nonim <rihan.nonim@gmail.com>
2021-01-12 19:07:07 +0700

    sd

---
+++ ApiParking/Data/User/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiParking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiParking.Data.User
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<UserModel> User { get; set; }
+    }
+}
---
+++ ApiParking/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiParking.Models
+{
+    public class UserModel 
+    {
+        [Key]
+        public int user_id { get; set; }
+        public string user_username { get; set; }
+        public string user_password { get; set; }
+
+        public string user_role { get; set; }
+
+        public DateTime user_craeted_at { get; set; }
+        public int users_sts { get; set; }
+    }
+}
